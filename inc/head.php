a57358bca01ed239ed61adbc053228335229adcb
clairedornic <50169132+clairedornic@users.noreply.github.com>
2022-06-22 13:51:53 +0200

    FEAT: add new font style

--- inc/head.php
+++ inc/head.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A la croisée des mondes</title>
+    <link rel="stylesheet" href="https://use.typekit.net/jdk6osy.css">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
